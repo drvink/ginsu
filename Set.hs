ae9a8d180f97dae891186ab5c7cc8812b247c856
John Meacham <john@repetae.net>
2005-01-09 08:29:18 +0000

    Add test cases and generalized set class

---
+++ Set.hs
@@ -0,0 +1,48 @@
+
+-- | This creates the boolean algebra of sets from any base boolean algebra.
+-- note that the sets created are 'true' sets in the mathematical sense, not
+-- the usual programatic aproximation.
+-- 
+-- A generalized set can be thought of as a map from keys to boolean values.
+-- perhaps the 'map with default' should be seperated out?
+
+module Boolean.Set where
+
+-- needs 'Map' from DData
+import Map hiding(member)
+
+data Set k v = Set v (Map k v) 
+
+instance Functor (Set k) where
+    fmap f (Set v map) = Set (f v) (Map.map f map)
+
+
+
+instance SemiBooleanAlgebra v => SemiBooleanAlgebra (Set k v) where
+    (&&) = combine (&&) 
+    (||) = combine (||)
+
+instance BooleanAlgebra v => BooleanAlgebra (Set k v) where
+    not x = fmap not x
+    true = Set true Map.empty
+    false = Set false Map.empty
+    xor = combine xor
+
+
+-- TODO 
+-- this is very inefficient, but we need a generalized unionWith to do it properly
+
+combine :: (v -> v -> v) -> Set k v -> Set k v -> Set k v
+combine f a@(Set da mapa) b@(Set db mapb) = Set (f da db) (Map.fromList $ map g $  keys mapa ++ keys mapb) where
+    g k = (k,f (member k a) (member k b) 
+
+empty = false
+single x (Set d map) = Set d (Map.insert x true map)
+insert x v (Set d map)  = Set d (Map.insert x v map)
+
+member :: k -> Set k v  -> v
+member k (Set d map) = case Map.lookup k map of
+    Just x -> x
+    Nothing -> d
+
+
---
+++ TestCases.hs
@@ -0,0 +1,60 @@
+-- | some test cases to test the Boolean.* modules.
+
+module Boolean.TestCases where
+
+import Boolean.Algebra 
+import Debug.QuickCheck
+import Prelude hiding(not, (&&), (||),and,or,any,all)
+--import ArbitraryInstances
+
+infix 4 ===
+               
+class Equality a where
+    (===) :: a -> a -> Bool
+    
+
+instance Equality Bool where
+    (===) = (==)
+
+instance Equality Int where
+    0 === 0 = True
+    0 === _ = False
+    _ === 0 = False
+    _ === _ = True
+
+instance (Equality a, Equality b) =>  Equality (a,b) where
+    (x,y) === (x',y') = (x === x' && y === y')
+
+--x === y = toBool x == toBool y
+
+-- semi-boolean properties
+prop_semi_and (x,y) = let z = x && y in z `elem` [x,y]
+prop_semi_or (x,y) = let z = x || y in z `elem` [x,y]
+prop_semi_b   (x,y) = (y && x) || x == x  
+
+-- we need equality for these to work
+prop_notnot x = (not (not x)) === x
+prop_true x = ((true && x) == x) && ((true || x) == true)
+prop_false x = ((false || x) == x)
+prop_false' x = ((false && x) == false) 
+prop_demorgan xs = (not (and xs)) === or (map not xs)
+prop_demorgan' xs = (not (or xs)) === and (map not xs)
+
+prop_truefalse true false x = ((true && x) == x) && ((true || x) == true) && ((false || x) == x) &&  ((false && x) == false) 
+
+main = do
+    quickCheck (\(x::Bool) -> prop_notnot x)
+    quickCheck (\(x::Int) -> prop_notnot x)
+    quickCheck (\(x::Int) -> prop_true x)
+    quickCheck (\(x::Int) -> prop_false x)
+    quickCheck (\(x::(Int,(Bool,Int))) -> prop_notnot x)
+    quickCheck (\(x::(Int,(Bool,Int))) -> prop_true x)
+    quickCheck (\(x::(Int,(Bool,Int))) -> prop_false x)
+    quickCheck (\(x::(Int,(Bool,Int))) -> prop_false' x)
+    quickCheck (\(x::[(Int,(Bool,Int))]) -> null x `trivial` prop_demorgan x)
+    quickCheck (\(x::[(Int,(Bool,Int))]) -> null x `trivial` prop_demorgan' x)
+    quickCheck $ prop_truefalse [3::Int] [] 
+    quickCheck $ prop_truefalse (Just True) Nothing 
+    quickCheck $ prop_truefalse ((Right True),[3::Int]) (Left (), []) 
+
+
