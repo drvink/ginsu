3367877b49e93c6d98b460a883e65030887ed5e2
John Meacham <john@repetae.net>
2007-05-09 05:58:52 +0000

    add precidence tracking to pprint class

--- PPrint.hs
+++ PPrint.hs
@@ -1,6 +1,9 @@
 
 -- | A Pretty printing class using multiparameter type classes for
 -- maximal generality with some useful instances.
+--
+-- the pprinted type comes as the last argument so newtype deriving can be used
+-- in more places.
 
 module Doc.PPrint where
 
@@ -10,16 +13,17 @@ import qualified Data.Map as Map
 
 class DocLike d => PPrint d a  where
     pprint ::  a -> d
+    pprintPrec :: Int -> a -> d
 
-    parPprint ::  a -> d
-    parPprint  = parens . pprint
+    pprintPrec _ a = pprint a
+    pprint a = pprintPrec 0 a
+
 
     pplist    ::  [a] -> d
     pplist    xs = brackets (hcat (punctuate comma (map pprint xs)))
 
-    pptuple   ::  [a] -> d
-    pptuple   xs = parens (hcat (punctuate comma (map pprint xs)))
-
+pprintParen :: PPrint d a => a -> d
+pprintParen = pprintPrec 11
 
 instance PPrint d a => PPrint d [a] where
     pprint  = pplist
@@ -47,14 +51,16 @@ instance (PPrint d a, PPrint d b) => PPrint d (a,b) where
   pprint (x,y) = parens (hsep [pprint x <> comma, pprint y])
 
 instance (PPrint d a, PPrint d b) => PPrint d (Either a b) where
-  pprint (Left x) = text "Left" <+> pprint x
-  pprint (Right x) = text "Right" <+> pprint x
+  pprintPrec n (Left x)  | n <= 9  = text "Left" <+> pprintPrec 10 x
+  pprintPrec n (Right x) | n <= 9  = text "Right" <+> pprintPrec 10 x
+  pprintPrec _ x = parens (pprint x)
 
 instance (PPrint d a, PPrint d b, PPrint d c) => PPrint d (a,b,c) where
   pprint (x,y,z) = parens (hsep [pprint x <> comma,
                                 pprint y <> comma,
                                 pprint z])
 
-
 instance (PPrint d a, PPrint d b) => PPrint d (Map.Map a b) where
     pprint m = vcat [ pprint x <+> text "=>" <+> pprint y | (x,y) <- Map.toList m]
+
+
